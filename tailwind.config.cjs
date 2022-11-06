f54be8ba96ce2e63841134d6caed5fe74993041d
Lucas Nguyen <lucasoAlt@outlook.com>
2022-11-06 17:12:03 +0100

    fixes

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,15 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				darkGrey: "#1F2232",
+				whiteYellow: "#F4E04D",
+				finePink: "#EB4B98",
+				brightBlue: "#D2FDFF",
+
+			},
+		},
 	},
 	plugins: [],
 }
